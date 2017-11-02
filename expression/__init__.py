514d8fe1fd357db993cf57fc04ae2f81b1430476
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-11-02 14:54:13 +0000

    Pylint: Alter name rules
    Replace 'disable' rule with improved regexes and allowing 'op' as
    a variable name.

--- expression/__init__.py
+++ expression/__init__.py
@@ -16,8 +16,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-# pylint: disable=invalid-name
-
 # Use Python 3 division
 from __future__ import division
 import ast
